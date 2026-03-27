75dc823f297f311b4e1ab8bbd01a99595ee2de87
flo986 <florian.mueller86@gmail.com>
2026-03-27 09:10:33 -0400

    Task 6

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -9,12 +8,13 @@ def index():
     return render_template("index.html")  
 
 @app.route("/emotionDetector")
-def RunSentimentAnalysis(input_text):    
-    emotions = emotion_detector(input_text)
+def RunSentimentAnalysis():    
+    textToAnalyze = request.args.get("textToAnalyze")
+    emotions = emotion_detector(textToAnalyze)
     dominant_emotion_key = max(emotions,key=emotions.get)
-    dominant_emotion = emotions[dominant_emotion_key]
+    
 
-    return f"For the given statement, the system response is {emotions}. The dominant emotion is {dominant_emotion}."
+    return f"For the given statement, the system response is 'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and 'sadness: {emotions['sadness']}'. The dominant emotion is {dominant_emotion_key}."
 
 if __name__ == "__main__":
     app.run(debug=True)
