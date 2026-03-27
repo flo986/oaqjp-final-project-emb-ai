6c9e3ef58626829db5dbed9629d966dc8bca32f6
flo986 <florian.mueller86@gmail.com>
2026-03-27 05:36:42 -0400

    Commit task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,5 +5,14 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } } 
     response = requests.post(url, json = myobj, headers=headers)
+    formatted_response = json.loads(response.text)
     
-    return response.text
+    anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+    disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+    fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+    joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+    sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+    
+    emotions_scores = {"anger": anger_score, "disgust": disgust_score, "fear": fear_score, "joy": joy_score, "sadness": sadness_score}
+    
+    return emotions_scores
